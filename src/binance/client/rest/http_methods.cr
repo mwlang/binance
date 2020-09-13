ea3b6d841e248050f9f64d1e3d59dc8b609979c3
mwlang <mwlang@cybrains.net>
2020-09-13 15:27:10 -0400

    memoize the connections

--- src/binance/client/rest/http_methods.cr
+++ src/binance/client/rest/http_methods.cr
@@ -12,36 +12,39 @@ module Binance::HttpMethods
       connection.{{method.id}} url_with_params
     end
 
-    def public_{{method.id}}(url, params : HTTP::Params)
-      connection = Cossack::Client.new(BASE_URL) do |client|
+    def public_connection
+      @public_connection ||= Cossack::Client.new(BASE_URL) do |client|
         client.headers["Content-Type"] = "application/json"
         client.headers["Accept"] = "application/json"
       end
-      http_{{method.id}} connection, url, params
     end
 
-    def verified_{{method.id}}(url, params : HTTP::Params)
-      raise "No API KEY assigned" if api_key.to_s.blank?
-      connection = Cossack::Client.new(BASE_URL) do |client|
+    def connection_with_api_key
+      @connection_with_api_key ||= Cossack::Client.new(BASE_URL) do |client|
         client.headers["Content-Type"] = "application/json"
         client.headers["Accept"] = "application/json"
         client.headers["X-MBX-APIKEY"] = api_key
       end
-      http_{{method.id}} connection, url, params
+    end
+
+    def public_{{method.id}}(url, params : HTTP::Params)
+      http_{{method.id}} public_connection, url, params
+    end
+
+    def verified_{{method.id}}(url, params : HTTP::Params)
+      raise "No API KEY assigned" if api_key.to_s.blank?
+
+      http_{{method.id}} connection_with_api_key, url, params
     end
 
     def signed_{{method.id}}(url, params : HTTP::Params)
       raise "No API KEY assigned" if api_key.to_s.blank?
       raise "No API SECRET assigned" if secret_key.to_s.blank?
-      connection = Cossack::Client.new(BASE_URL) do |client|
-        client.headers["Content-Type"] = "application/json"
-        client.headers["Accept"] = "application/json"
-        client.headers["X-MBX-APIKEY"] = api_key
-      end
+
       params["timestamp"] = Time.utc.to_unix_ms.to_s
       params["signature"] = hmac to_query(params)
 
-      http_{{method.id}} connection, url, params
+      http_{{method.id}} connection_with_api_key, url, params
     end
 
   {% end %}
